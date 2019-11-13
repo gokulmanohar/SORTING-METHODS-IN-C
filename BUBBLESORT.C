5c9f8bf484477a0b008673d607f11a9a284f8038
Gokul Manohar <44633074+gokulmanohar@users.noreply.github.com>
2019-11-13 20:25:34 +0530

    Add files via upload

---
+++ BUBBLESORT.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int i, j, n, a[10], min, temp;
+    clrscr();
+    printf("ENTER THE SIZE - ");
+    scanf("%d",&n);
+    printf("ENTER THE ARRAY\n");
+    for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+    printf("ENTERED ARRAY\n");
+    for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+    //sorting
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+            
+    }
+}
